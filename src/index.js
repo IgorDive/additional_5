fea744101613a1b4d17a2cf1c4bb3d6a3acba091
IgorDive <ihar.kazlovich@gmail.com>
2018-02-27 18:34:57 +0300

    first modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let conf = bracketsConfig.map( (elem) => ( /\d/.test(bracketsConfig[0][0]) )?`${elem.join('')}`:`\\${elem.join('\\')}` ).join('|');
+  let RE = new RegExp(conf, 'g');
+  let currentStr = str;
+
+  while ( RE.test(currentStr) ) {
+    currentStr = currentStr.replace( RE, '');
+  }
+  return !Boolean(currentStr);
 }
